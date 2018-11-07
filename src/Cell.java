7dbadc4e4e9382d6dc3fa63ff58a973819aa488f
sudri‘git config --global user.name sudri“ <sudrizzz@gmail.com>
2018-11-07 16:05:33 +0800

    version0.4

--- src/Cell.java
+++ src/Cell.java
@@ -8,7 +8,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 public class Cell {
 
@@ -30,7 +30,7 @@ public class Cell {
             e.printStackTrace();
         }
         Document document = Jsoup.parse(sb.toString());
-        HashMap map = recursiveDFS(document);
+        LinkedHashMap map = recursiveDFS(document);
 
         return new JSONObject(map);
     }
@@ -39,8 +39,8 @@ public class Cell {
     int mainCol = 0; // 主表列
     int detailCount = 0;
     ArrayList mainList = new ArrayList(); // 主表list
-    HashMap emaintable = new HashMap();
-    HashMap etables = new HashMap();
+    LinkedHashMap emaintable = new LinkedHashMap();
+    LinkedHashMap etables = new LinkedHashMap();
 
     /**
      * 递归取出所有的节点信息，包括字段显示名和标题
@@ -48,7 +48,7 @@ public class Cell {
      * @param element
      * @return
      */
-    public HashMap recursiveDFS(Element element) {
+    public LinkedHashMap recursiveDFS(Element element) {
 
         if (element.tag().toString().equals("tr") && element.child(0).tag().toString().equals("td")) {
             mainRow++; // 下一行
@@ -59,14 +59,17 @@ public class Cell {
             // 如果td里面是table，则认为其是明细表，进行特殊处理
             if (element.select("table").size() != 0) {
                 for (Element e : element.select("table")) {
-                    etables.put("detail", detailTableAnalyse(e));
+                    LinkedHashMap map = detailTableAnalyse(e);
+                    if (map.size() != 0) {
+                        etables.put("detail_" + ++detailCount, map);
+                    }
                 }
                 return etables;
 
                 // 如果td里面是input框，则针对input框进行处理
             } else if (element.select("input").size() != 0) {
                 for (int i = 0; i < element.select("input").size(); i++) {
-                    HashMap inputMap = new HashMap();
+                    LinkedHashMap inputMap = new LinkedHashMap();
                     String colspan = element.attr("colspan");
                     String name = element.select("input").get(i).attr("name");
                     String value = element.select("input").get(i).attr("value");
@@ -78,7 +81,7 @@ public class Cell {
                     inputMap.put("etype", "3");
                     inputMap.put("evalue", value);
 
-                    // 如果tr上有display:none属性，则记录下该属性
+                    // 如果tr上有display:none属性，则记录下该显示属性
                     if (element.parent().attr("style").equals("display: none")) {
                         inputMap.put("display", "none");
                     }
@@ -92,9 +95,33 @@ public class Cell {
                 } else {
                     mainCol++;
                 }
-                // 否则就为普通的td
-            } else {
-                HashMap inputMap = new HashMap();
+
+                // 如果里面有strong标签，则作为标题处理
+            } else if (element.select("strong").size() != 0) {
+                LinkedHashMap map = new LinkedHashMap();
+                String color = element.parent().selectFirst("font").attr("color");
+                String size = element.parent().selectFirst("font").attr("size");
+                String weight = element.parent().selectFirst("font").attr("weight");
+                String colspan = element.attr("colspan");
+                String width = element.attr("width");
+                String align = element.attr("align");
+                String text = element.text();
+                map.put("id", mainRow + "," + 0);
+                map.put("color", color);
+                map.put("size", size);
+                map.put("weight", weight);
+                map.put("colspan", colspan);
+                map.put("width", width);
+                map.put("align", align);
+                map.put("text", text);
+                ArrayList list = (ArrayList) etables.get("title");
+                list.add(map);
+                etables.put("title", list);
+                mainRow++;
+                return etables;
+
+            } else { // 否则就为普通的td
+                LinkedHashMap inputMap = new LinkedHashMap();
                 String colspan = element.attr("colspan");
                 String width = element.attr("width");
                 String align = element.attr("align");
@@ -106,8 +133,8 @@ public class Cell {
                         inputMap.put("field", name.substring(5));
                     }
                 }
-                inputMap.put("evalue", text);
                 inputMap.put("id", mainRow + "," + mainCol);
+                inputMap.put("evalue", text);
                 inputMap.put("colspan", colspan.equals("") ? "1" : colspan);
                 inputMap.put("rowspan", "1");
                 inputMap.put("width", width);
@@ -141,8 +168,8 @@ public class Cell {
      * @param doc
      * @return
      */
-    public HashMap getCssAndScript(Document doc) {
-        HashMap map = new HashMap();
+    public LinkedHashMap getCssAndScript(Document doc) {
+        LinkedHashMap map = new LinkedHashMap();
         int i = 0;
         for (Element e : doc.select("link")) {
             map.put(++i, e);
@@ -156,8 +183,6 @@ public class Cell {
 
     int detailRow = 0; // 明细表行
     int detailCol = 0; // 明细表列
-    ArrayList detailList = new ArrayList();
-    HashMap detailMap = new HashMap();
 
     /**
      * 单独处理明细表
@@ -165,44 +190,52 @@ public class Cell {
      * @param element
      * @return
      */
-    public HashMap detailTableAnalyse(Element element) {
+    public LinkedHashMap detailTableAnalyse(Element element) {
+        ArrayList detailList = new ArrayList();
+        LinkedHashMap detailMap = new LinkedHashMap();
+
         if (element.select("strong").size() != 0 && !(element.text().trim().equals(""))) {
+
             // 表单标题
-            for (Element e : element.select("strong").parents()) {
+            for (Element e : element.children()) {
                 if ((e.text().trim().equals("")) || (e.text().trim().equals("&nbsp;"))) {
-                    continue;
+                    return etables;
                 }
-//                HashMap map = new HashMap();
-//                String color = e.attr("color");
-//                String size = e.attr("size");
-//                String weight = e.attr("weight");
-//                String colspan = e.attr("colspan");
-//                String width = e.attr("width");
-//                String align = e.attr("align");
-//                String text = e.text();
-//                map.put("id", color);
-//                map.put("color", color);
-//                map.put("size", size);
-//                map.put("weight", weight);
-//                map.put("colspan", colspan);
-//                map.put("width", width);
-//                map.put("align", align);
-//                map.put("text", text);
-//                detailList.add(map);
-//                detailMap.put("ec", detailList);
-//                detailCol++;
-                return detailMap;
+                if (e.tag().toString().equals("strong")) {
+                    e = e.parent();
+                    LinkedHashMap map = new LinkedHashMap();
+                    ArrayList titleMap = new ArrayList();
+                    String color = e.attr("color");
+                    String size = e.attr("size");
+                    String weight = e.attr("weight");
+                    String colspan = e.attr("colspan");
+                    String width = e.attr("width");
+                    String align = e.attr("align");
+                    String text = e.text();
+                    map.put("id", mainRow + "," + 0);
+                    map.put("color", color);
+                    map.put("size", size);
+                    map.put("weight", weight);
+                    map.put("colspan", colspan);
+                    map.put("width", width);
+                    map.put("align", align);
+                    map.put("text", text);
+                    titleMap.add(map);
+                    etables.put("title", titleMap);
+                    mainRow++;
+                    return etables;
+                }
             }
         } else {
             for (Element detailtd : element.select("td")) {
                 if (detailtd.select("input").size() != 0) {
                     for (int i = 0; i < detailtd.select("input").size(); i++) {
-                        HashMap map = new HashMap();
+                        LinkedHashMap map = new LinkedHashMap();
                         String colspan = detailtd.attr("colspan");
                         String name = detailtd.select("input").get(i).attr("name");
                         String value = detailtd.select("input").get(i).attr("value");
-                        if (detailRow == 1) map.put("attr", "字段");
                         map.put("id", detailRow + "," + detailCol);
+                        if (detailRow == 1) map.put("attr", "content");
                         map.put("rowspan", 1);
                         map.put("field", name.substring(5));
                         map.put("fieldtype", "text");
@@ -214,12 +247,12 @@ public class Cell {
                     }
                 } else if (!(detailtd.text().trim().equals("")) && !(detailtd.text().trim().equals("&nbsp;"))) {
                     // 否则就为普通的td
-                    HashMap map = new HashMap();
+                    LinkedHashMap map = new LinkedHashMap();
                     String width = detailtd.attr("width");
                     String align = detailtd.attr("align");
                     String text = detailtd.text();
-                    if (detailRow == 0) map.put("attr", "表头");
                     map.put("id", detailRow + "," + detailCol);
+                    if (detailRow == 0) map.put("attr", "title");
                     map.put("rowspan", 1);
                     map.put("width", width);
                     map.put("align", align);
@@ -230,14 +263,14 @@ public class Cell {
                     detailCol++;
                 }
                 if (detailtd.nextElementSibling() == null) {
-                    if (detailRow == 1) detailRow =0; // 明细表只有两行，一行为表头，一行为字段
+                    if (detailRow == 1) detailRow = 0; // 明细表只有两行，一行为表头，一行为字段
                     else detailRow++; // 下一行
                     detailCol = 0; // 列归零
                 }
             }
             return detailMap;
         }
-        for (Element e : element.select("td")) {
+        for (Element e : element.children()) {
             detailTableAnalyse(e);
         }
         return detailMap;
